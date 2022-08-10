ac9a24a094163ea013ab51d3bba445661f2f5306
LucasMendes013 <104436533+LucasMendes013@users.noreply.github.com>
2022-08-10 10:56:05 -0300

    Manipulação de Date
    Get and push in your respectives senses

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const number = 1
 function verifica(){
   var input = document.getElementById('numero').value
-  try{
-    if(input == '') throw ': Informe um valor'
+  try{  //Execute todas as vezes
+    if(input == '') throw ': Informe um valor' //Throw é como um 'faça', que cabe dentro do try
     if(isNaN(input)) throw ': Informe um número'
     input = Number(input)
     if(input < 10) throw ': O número é menor que 10'
     if(input > 20) throw ': O número é maior que 10'
 
     document.getElementById('msg').innerHTML = 'Tudo correto, você digitou ' + input
-  } catch(erro){
+  } catch(erro){ //Caso o acima não tenha algum erro
     document.getElementById('msg').innerHTML = 'Erro' +erro
-  } finally {
+  } finally { //Mesmo que já tenha sido executado e aparecido o erro, isso ocorrerá
     document.getElementById('numero').value = ''
   }
 }
